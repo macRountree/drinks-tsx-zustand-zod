fd421f761a5ea104d3938ccec8a7efde53ece6d5
MacRountree <macoy1492@hotmail.com>
2024-09-01 12:37:57 -0700

    feat: Update FavoritesPage to display favorite recipes
    The FavoritesPage component has been updated to display the user's favorite recipes. This change enhances the user experience by allowing them to easily view and access their favorite recipes.

--- src/views/FavoritesPage.tsx
+++ src/views/FavoritesPage.tsx
@@ -1,7 +1,28 @@
+import {useGlobalStore} from '../stores/useGlobalStore';
+import {RecipesCard} from '../components/RecipesCard';
+import {useMemo} from 'react';
+
 export const FavoritesPage = () => {
+  const favorites = useGlobalStore(state => state.favorites);
+  const hasFavs = useMemo(() => favorites.length, [favorites]);
   return (
     <>
-      <h1>Favirutes</h1>
+      <h1 className="text-center text-8xl font-extrabold">
+        {' '}
+        Favorites Recipes{' '}
+      </h1>
+      {hasFavs ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-10 gap-10">
+          {favorites.map(fav => (
+            <RecipesCard key={fav.idDrink} recipe={fav} />
+          ))}
+        </div>
+      ) : (
+        <p className="text-4xl text-center bg-rose-600 p-2 rounded-md text-white font-extrabold mt-6">
+          {' '}
+          Not Favorites Recipes.{' '}
+        </p>
+      )}
     </>
   );
 };
