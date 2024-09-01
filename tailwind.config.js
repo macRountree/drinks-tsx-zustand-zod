1417b2d65f65cacc33e33a1f4bf2cc84d30d0d52
MacRountree <macoy1492@hotmail.com>
2024-09-01 14:45:45 -0700

    chore: Update background image URL in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     extend: {
       backgroundImage: {
         header:
-          'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("./bg_drinks.jpg")',
+          'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("/bg_drinks.jpg")',
       },
     },
   },
